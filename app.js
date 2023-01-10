c40bdf65734ee3651b91a2fd3ddb6bc66809df8c
SubbarajuGitHub <subba3295@gmail.com>
2023-01-10 07:22:13 +0000

    test case not passing

--- app.js
+++ app.js
@@ -38,9 +38,75 @@ app.post("/movies/", async (request, response) => {
   const { directorId, movieName, leadActor } = request.body;
   const UpdateMovie = `
   insert into
-  movie (directorId, movieName, leadActor)
+  movie (director_id, movie_name, lead_actor)
+  values
   (${directorId}, ${movieName} ,${leadActor});
     `;
-  const NewMovieArray = await db.get(UpdateMovie);
-  response.send(NewMovieArray);
+  const NewMovieArray = await db.run(UpdateMovie);
+  response.send("Movie Successfully Add");
 });
+
+app.get("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+  const SingleMovie = `
+    SELECT 
+      * 
+    FROM 
+      movie 
+    WHERE 
+      movie_id = ${movie_id};`;
+  const SingleMovieArray = await db.get(SingleMovie);
+  response.send(SingleMovieArray);
+});
+
+app.put("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+  const { directorId, movie_id, leadActor } = request.body;
+  const updateMovie = `
+    update
+    movie
+    set
+    director_id='${directorId}',
+    movie_id='${movie_id}'',
+    lead_actor='${leadActor}',
+    where
+    movie_id='${movie_id};
+    `;
+  await db.run(updateMovie);
+  response.send("Movie Details Updated");
+});
+
+app.delete("/movies/:movieId/", async (request, response) => {
+  const { movie_id } = request.params;
+  const DeleteMovie = `
+    delete
+    from movie
+    where movie_id='${movie_id}`;
+  await db.run(DeleteMovie);
+  response.send("Movie Removed");
+});
+
+app.get("/directors/", async (request, response) => {
+  const AllDirectors = `
+    select *
+    from 
+    director
+    ;`;
+  const DirectersArray = await db.all(AllDirectors);
+  response.send(DirectersArray);
+});
+
+app.get("/directors/:directorId/movies/", async (request.response) => {
+  const { director_id } = request.params;
+  const directedMovies = `
+    select
+    movie_name	
+    from
+    movie
+    where director_id='${director_id}';
+    `;
+  const directorArrays = await db.all(directedMovies);
+  response.send(directorArrays);
+});
+
+module.exports = app;
